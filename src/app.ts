afa48e750d6f93f9728b0e8409e7e47eabdef9e3
kimjuno97 <jh241093@gmail.com>
2023-01-12 13:07:48 +0900

    feat: app.ts, server.ts 서버생성,  서버실행 코드 분리

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,21 @@
 // 타입스크립트를 쓰면 express 선언전에 dotenv.config 해줘야함
 import dotenv from 'dotenv';
 dotenv.config();
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
 import routes from './routes';
 
-const app = express();
+const createApp = () => {
+	const app = express();
 
-app.use(cors());
-app.use(morgan('combined'));
-app.use(express.json());
-app.use(routes);
+	app.use(cors());
+	app.use(morgan('combined'));
+	app.use(express.json());
+	app.use(routes);
 
-app.get('/ping', (req: Request, res: Response, next: NextFunction) => {
-	res.json({ message: 'pong' });
-});
+	return app;
+};
 
-app.listen(3000, () => {
-	console.log('Server listening on port: 3000');
-});
+export default createApp;
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import createApp from './app';
+import { Request, Response } from 'express';
+
+const startServer = async () => {
+	const app = createApp();
+
+	app.get('/ping', (req: Request, res: Response) => {
+		res.json({ message: 'pong' });
+	});
+
+	app.listen(3000, () => {
+		console.log('Server listening on port: 3000');
+	});
+};
+
+startServer();
